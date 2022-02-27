14ee4a66194306c5ebf23cedd18309321b2c4ce2
cy <51856506+ChiYouIc@users.noreply.github.com>
2022-02-27 14:32:36 +0800

    更新参数校验包

--- personal-start-validator/src/main/java/cn/cy/validator/Update.java
+++ personal-start-validator/src/main/java/cn/cy/validator/Update.java
@@ -1,9 +1,11 @@
 package cn.cy.validator;
 
+import javax.validation.groups.Default;
+
 /**
  * @Author: 友叔
  * @Date: 2021/12/20 14:44
  * @Description: 用户参数分组检验（更新）
  */
-public interface Update {
+public interface Update extends Default {
 }
--- personal-start-validator/src/main/java/cn/cy/validator/ValidatorExceptionHandler.java
+++ personal-start-validator/src/main/java/cn/cy/validator/ValidatorExceptionHandler.java
@@ -7,13 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 /**
  * @Author: 友叔
@@ -24,42 +27,60 @@ import javax.validation.ConstraintViolationException;
 @ControllerAdvice
 public class ValidatorExceptionHandler extends AbstractExceptionHandleAdvice implements Ordered {
 
-	@ExceptionHandler({MethodArgumentNotValidException.class})
-	public ResponseEntity<FailedResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		BindingResult bindingResult = e.getBindingResult();
-		StringBuilder sb = new StringBuilder("校验失败:");
-		for (FieldError fieldError : bindingResult.getFieldErrors()) {
-			sb.append(fieldError.getField()).append(": ").append(fieldError.getDefaultMessage()).append(", ");
-		}
+    /**
+     * <p> {@code BindException}: 处理 form data方式调用接口校验失败抛出的异常
+     *
+     * <p> {@code MethodArgumentNotValidException}: 处理 json 请求体调用接口校验失败抛出的异常
+     *
+     * @param e 异常
+     * @return 处理结果
+     * @see BindException
+     * @see MethodArgumentNotValidException
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    public ResponseEntity<FailedResponse> handleBindExceptionException(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        StringBuilder sb = new StringBuilder("校验失败:");
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            sb.append(fieldError.getField()).append(": ").append(fieldError.getDefaultMessage()).append(", ");
+        }
 
-		String msg = sb.toString();
-		FailedResponse response = FailedResponse.builder()
-				.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-				.msg(msg)
-				.exception(ExceptionUtil.stacktraceToString(e))
-				.build();
+        String msg = sb.toString();
+        FailedResponse response = FailedResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .msg(msg)
+                .exception(ExceptionUtil.stacktraceToString(e))
+                .build();
 
-		log.error(e.getMessage());
-		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+        log.error(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@ExceptionHandler({ConstraintViolationException.class})
-	public ResponseEntity<FailedResponse> handleConstraintViolationException(ConstraintViolationException e) {
+    /**
+     * 处理单个参数校验失败抛出的异常
+     *
+     * @param e 异常
+     * @return 处理结果
+     */
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<FailedResponse> handleConstraintViolationException(ConstraintViolationException e) {
 
-		FailedResponse response = FailedResponse.builder()
-				.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-				.msg(e.getMessage())
-				.exception(ExceptionUtil.stacktraceToString(e))
-				.build();
+        String msg = e.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(","));
 
-		log.error(e.getMessage());
+        FailedResponse response = FailedResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .msg(msg)
+                .exception(ExceptionUtil.stacktraceToString(e))
+                .build();
 
-		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+        log.error(msg);
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 
-	@Override
-	public int getOrder() {
-		return 99;
-	}
+    @Override
+    public int getOrder() {
+        return 99;
+    }
 }
