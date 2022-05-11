f6cbfb525b86c3e3e80c3a24d35b0481d824d529
you <2419267527@qq.com>
2022-05-11 19:12:59 +0800

    更新全局异常处理

--- personal-start-web/src/main/java/cn/cy/web/advice/GlobalExceptionHandleAdvice.java
+++ personal-start-web/src/main/java/cn/cy/web/advice/GlobalExceptionHandleAdvice.java
@@ -53,6 +53,7 @@ public class GlobalExceptionHandleAdvice extends AbstractExceptionHandleAdvice i
                 .exception(ExceptionUtil.stacktraceToString(e))
                 .build();
         log.error(e.getMessage());
+        e.printStackTrace();
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
