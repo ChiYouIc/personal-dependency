65f79f423529c3b3619ca050fbf5225bdbfa80d6
you <2419267527@qq.com>
2022-06-17 16:02:28 +0800

    去掉lombok

--- personal-start-sso/src/main/java/cn/cy/sso/config/properties/SsoProperties.java
+++ personal-start-sso/src/main/java/cn/cy/sso/config/properties/SsoProperties.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @description: SSO 配置
  * @create: 2021-08-08 13:16
  **/
-@Data
 @ConfigurationProperties(prefix = SsoProperties.PREFIX)
 public class SsoProperties {
     protected final static String PREFIX = "sso-core";
@@ -21,5 +20,29 @@ public class SsoProperties {
 
     public String logoutUrl() {
         return "/logout";
+    }
+
+    public ServerType getType() {
+        return type;
+    }
+
+    public void setType(ServerType type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAppCode() {
+        return appCode;
+    }
+
+    public void setAppCode(String appCode) {
+        this.appCode = appCode;
     }
 }
--- personal-start-sso/src/main/java/cn/cy/sso/model/RequestPath.java
+++ personal-start-sso/src/main/java/cn/cy/sso/model/RequestPath.java
@@ -1,13 +1,10 @@
 package cn.cy.sso.model;
 
-import lombok.Data;
-
 /**
  * @Author: 友
  * @Date: 2022/6/16 18:12
  * @Description:
  */
-@Data
 public class RequestPath {
     /**
      * 请求处理方法
@@ -29,4 +26,35 @@ public class RequestPath {
      */
     private int paramCount = 0;
 
+    public String getHandleMethod() {
+        return handleMethod;
+    }
+
+    public void setHandleMethod(String handleMethod) {
+        this.handleMethod = handleMethod;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public void setRequestMethod(String requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public int getParamCount() {
+        return paramCount;
+    }
+
+    public void setParamCount(int paramCount) {
+        this.paramCount = paramCount;
+    }
 }
